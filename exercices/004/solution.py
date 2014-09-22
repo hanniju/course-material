c054fc77a5401b470e4e59a5764ebd8f9abf2900
hanniju <hannequin.j@gmail.com>
2014-09-22 15:27:49 +0200

    fifth commmit

---
+++ exercices/004/solution.py
@@ -0,0 +1,7 @@
+# os.path.realpath(__file__)
+
+import os
+print os.path.basename(__file__)
+
+
+# solution found on: http://stackoverflow.com/questions/50499/in-python-how-do-i-get-the-path-and-name-of-the-file-that-is-currently-executin
