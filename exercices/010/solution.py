e1c84eb7d4730751c720eac3fa74b2dee51b45ac
hanniju <hannequin.j@gmail.com>
2014-09-24 17:39:04 +0200

    Done

---
+++ exercices/010/solution.py
@@ -0,0 +1,12 @@
+#Print "hello world" a hundred times on new lines
+
+i = 0
+
+while i <= 100:
+#tif i <= 100:
+    print("Hello world")
+    i += 1
+    continue
+else:
+    print("over and out")
+
